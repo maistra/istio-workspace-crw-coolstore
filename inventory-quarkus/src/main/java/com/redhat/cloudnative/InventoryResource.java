2f0c2cc653e315e23379b66daebfff61f10e473c
Bartosz Majsak <bartosz.majsak@gmail.com>
2022-02-14 11:49:12 +0100

    fix: sets inventory quantity to zero if not found (#20)

--- inventory-quarkus/src/main/java/com/redhat/cloudnative/InventoryResource.java
+++ inventory-quarkus/src/main/java/com/redhat/cloudnative/InventoryResource.java
@@ -21,6 +21,10 @@ public class InventoryResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Inventory getAvailability(@PathParam("itemId") String itemId) {
         Inventory inventory = em.find(Inventory.class, itemId);
+        if (inventory == null) {
+            inventory = new Inventory();
+            inventory.setQuantity(0);
+        }
         return inventory;
     }
 }
